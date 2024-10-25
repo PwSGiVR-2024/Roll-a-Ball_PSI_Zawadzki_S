4acff3db8696a84457b5b4c44792d17504c1c06c
ZawasIT <zawadzkiszymonit@gmail.com>
2024-10-25 21:08:09 +0200

    Poprawienie kolorów Main Menu, dodanie przykładowego OptionsPanel

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
     public GameObject optionsPanel;
+    public GameObject menuPanel;
 
+    public GameObject startButton;
+    public GameObject exitButton;
+    public GameObject optionsButton;
+
+    public Toggle toggleLayout;
 
     public void StartGame()
     {
@@ -17,14 +25,15 @@ public class MenuScript : MonoBehaviour
     public void ShowOptions(bool isActive)
     {
         Debug.Log("Poka¿ opcje gry");
+        startButton.SetActive(false);
+        exitButton.SetActive(false);
         optionsPanel.SetActive(!optionsPanel.activeInHierarchy);
 
-    }
-    public void CloseOptions()
-    {
-        Debug.Log("Zamknij opcje gry");
-        optionsPanel.SetActive(false);
-
+        if (!optionsPanel.activeInHierarchy)
+        {
+            startButton.SetActive(true);
+            exitButton.SetActive(true);
+        }
     }
 
     public void ExitGame()
@@ -33,7 +42,30 @@ public class MenuScript : MonoBehaviour
         Application.Quit();
     }
 
+    public void OnToggleChanged()
+    {
+        Image imageMenuPanel = menuPanel.GetComponent<Image>();
+        Image imageStartButton = startButton.GetComponent<Image>();
+        Image imageExitButton = exitButton.GetComponent<Image>();
+        Image imageOptionsButton = optionsButton.GetComponent<Image>();
+        Image imageToggleLayout = toggleLayout.transform.Find("Background").GetComponent<Image>();
+        //Text toggleLayoutText = toggleLayout.GetComponentInChildren<Text>();
 
-    
-
+        if (toggleLayout.isOn)
+        {
+            imageMenuPanel.color = new Color(30f / 255f, 55f / 255f, 153f / 255f, 100f / 255);
+            imageStartButton.color = new Color(250f / 255f, 211f / 255f, 144f / 255f, 1f);
+            imageExitButton.color = new Color(229f / 255f, 80f / 255f, 57f / 255f, 1f);
+            imageOptionsButton.color = new Color(184f / 255f, 233f / 255f, 148f / 255f, 1f);
+            imageToggleLayout.color = new Color(130f / 255f, 204f / 255f, 221f / 255f);
+        }
+        else
+        {
+            imageMenuPanel.color = new Color(30f / 255f, 55f / 255f, 153f / 255f, 150f / 255);
+            imageStartButton.color = new Color(246f / 255f, 185f / 255f, 59f / 255f, 1f);
+            imageExitButton.color = new Color(235f / 255f, 47f / 255f, 6f / 255f, 1f);
+            imageOptionsButton.color = new Color(120f / 255f, 224f / 255f, 143f / 255f, 1f);
+            imageToggleLayout.color = new Color(60f / 255f, 99f / 255f, 130f / 255f);
+        }
+    }
 }
